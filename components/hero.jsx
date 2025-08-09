18940ed04a07c9543b80f528f117a16b4cb26c51
SHIVAM PRAJAPATI <151209521+Shivam1237@users.noreply.github.com>
2025-08-09 10:36:57 +0530

    Update hero.jsx

--- components/hero.jsx
+++ components/hero.jsx
@@ -50,7 +50,7 @@ const HeroSection = () => {
               Get Started
             </Button>
           </Link>
-          <Link href="/dashboard">
+          <Link>
             <Button size="lg" variant="outline" className="px-8">
               Watch Demo
             </Button>
